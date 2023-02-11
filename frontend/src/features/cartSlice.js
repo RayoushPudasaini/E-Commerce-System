f2437626d71d98d1db55c1816a98844cf89fe197
HP <HP@LAPTOP-GQLCABM5>
2023-02-11 21:21:48 +0545

    Add to cart page is completed

--- frontend/src/features/cartSlice.js
+++ frontend/src/features/cartSlice.js
@@ -39,9 +39,83 @@ const cartSlice = createSlice({
 
       localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
+    removeFromCart(state, action) {
+      const nextCartItems = state.cartItems.filter(
+        (cartItems) => cartItems.id !== action.payload.id
+      );
+
+      state.cartItems = nextCartItems;
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+
+      toast.error(`${action.payload.name} Remove from cart`, {
+        position: "top-right",
+        autoClose: 2000,
+        theme: "colored",
+      });
+    },
+
+    decreaseCart(state, action) {
+      const itemIndex = state.cartItems.findIndex(
+        (cartItems) => cartItems.id === action.payload.id
+      );
+
+      if (state.cartItems[itemIndex].cartQuantity > 1) {
+        state.cartItems[itemIndex].cartQuantity -= 1;
+
+        toast.info(`Decreased ${action.payload.name} cart quantity`, {
+          position: "top-right",
+          autoClose: 2000,
+          theme: "colored",
+        });
+      } else if (state.cartItems[itemIndex].cartQuantity === 1) {
+        const nextCartItems = state.cartItems.filter(
+          (cartItems) => cartItems.id !== action.payload.id
+        );
+
+        state.cartItems = nextCartItems;
+
+        toast.error(`${action.payload.name} Remove from cart`, {
+          position: "top-right",
+          autoClose: 2000,
+          theme: "colored",
+        });
+      }
+
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+    clearCart(state, action) {
+      state.cartItems = [];
+      toast.error(`Cart cleared`, {
+        position: "top-right",
+        autoClose: 2000,
+        theme: "colored",
+      });
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+    getTotals(state, action) {
+      let { total, quantity } = state.cartItems.reduce(
+        (cartTotal, cartItem) => {
+          const { price, cartQuantity } = cartItem;
+          const itemTotal = price * cartQuantity;
+
+          cartTotal.total += itemTotal;
+          cartTotal.quantity += cartQuantity;
+
+          return cartTotal;
+        },
+        {
+          total: 0,
+          quantity: 0,
+        }
+      );
+
+      state.cartTotalQuantity = quantity;
+      state.cartTotalAmount = total;
+    },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, decreaseCart, clearCart, getTotals } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
