010e9d9fe205ad94f918aeb4f1380032b6d41d49
RayoushPudasaini <101966343+RayoushPudasaini@users.noreply.github.com>
2023-12-15 17:26:45 +0545

    Update products.js

--- backend/products.js
+++ backend/products.js
@@ -5,7 +5,7 @@ const products = [
     desc: "Take to the Streets ",
     price: 999,
     image:
-      "https://res.cloudinary.com/danpzdlik/image/upload/v1677598431/images/nike_retro_t2pidp.png",
+      "Add link",
     brand: "Nike",
   },
   {
@@ -14,7 +14,7 @@ const products = [
     desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. ",
     price: 699,
     image:
-      "https://res.cloudinary.com/danpzdlik/image/upload/v1677598440/images/vapor_max_ippioh.png",
+      "Add link",
 
     brand: "Nike",
   },
@@ -24,7 +24,7 @@ const products = [
     desc: "Limited product",
     price: 399,
     image:
-      "https://res.cloudinary.com/danpzdlik/image/upload/v1677598442/images/goldstar_eysk4v.jpg",
+      "add link",
     brand: "Goldstar",
   },
   {
@@ -33,7 +33,7 @@ const products = [
     desc: "Limited product",
     price: 399,
     image:
-      "https://res.cloudinary.com/danpzdlik/image/upload/v1677599205/images/4_fszcyc.jpg",
+      "add link",
     brand: "Goldstar",
   },
   {
@@ -42,7 +42,7 @@ const products = [
     desc: "Limited product",
     price: 399,
     image:
-      "https://res.cloudinary.com/danpzdlik/image/upload/v1677599264/images/2_ur5tr7.jpg",
+      "Add link",
     brand: "Goldstar",
   },
 ];
